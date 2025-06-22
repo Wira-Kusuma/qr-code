f5183be6b56a3fe3a50415a6340b6fd5c5584771
Wira Kusuma Phandawa <wirakusuma5566@gmail.com>
2025-06-22 22:45:24 +0700

    animation when load

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    document.querySelector("main").classList.add("effect");
+    document.querySelector("section").classList.add("effect");
+    document.querySelector("img").classList.add("effect");
+    document.querySelector("h1").classList.add("effect");
+    document.querySelector("footer").classList.add("effect");
+}
